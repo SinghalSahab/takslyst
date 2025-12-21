505881a35bbc7191ddbbeca49e37176ac363f1a6
SinghalSahab <prakharsinghal10a@gmail.com>
2025-12-21 20:04:02 +0530

    backend routes done

---
+++ server/src/index.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+// Import Routes
+import projectRoutes from './routes/projectRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
+import searchRoutes from './routes/searchRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import teamRoutes from './routes/teamRoutes.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('common'));
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
+app.use("/search", searchRoutes);
+app.use("/users", userRoutes);
+app.use("/teams", teamRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
