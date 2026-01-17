8107d52a4e837a779df74e15dff18b7bb8b00725
SinghalSahab <prakharsinghal10a@gmail.com>
2026-01-17 17:36:14 +0530

    done till home

--- client/state/api.ts
+++ client/state/api.ts
@@ -153,17 +153,17 @@ export const api = createApi({
         { type: "Tasks", id: taskId },
       ],
     }),
-//     getUsers: build.query<User[], void>({
-//       query: () => "users",
-//       providesTags: ["Users"],
-//     }),
-//     getTeams: build.query<Team[], void>({
-//       query: () => "teams",
-//       providesTags: ["Teams"],
-//     }),
-//     search: build.query<SearchResults, string>({
-//       query: (query) => `search?query=${query}`,
-//     }),
+    getUsers: build.query<User[], void>({
+      query: () => "users",
+      providesTags: ["Users"],
+    }),
+    getTeams: build.query<Team[], void>({
+      query: () => "teams",
+      providesTags: ["Teams"],
+    }),
+    search: build.query<SearchResults, string>({
+      query: (query) => `search?query=${query}`,
+    }),
    }),
 });
 
@@ -173,9 +173,9 @@ export const {
   useGetTasksQuery,
   useCreateTaskMutation,
   useUpdateTaskStatusMutation,
-//   useSearchQuery,
-//   useGetUsersQuery,
-//   useGetTeamsQuery,
+  useSearchQuery,
+  useGetUsersQuery,
+  useGetTeamsQuery,
 //   useGetTasksByUserQuery,
 //   useGetAuthUserQuery,
 } = api;
