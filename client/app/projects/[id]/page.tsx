06ddd12fc84e842d54278c12c3f09f8e176eeecc
SinghalSahab <prakharsinghal10a@gmail.com>
2026-05-15 12:29:24 +0530

    more error fix

--- client/app/projects/[id]/page.tsx
+++ client/app/projects/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState,use } from "react";
 import ProjectHeader from "@/app/projects/ProjectHeader";
 import Board from "@/app/projects/BoardView";
 import List from "@/app/projects/ListView";
@@ -9,11 +9,11 @@ import Table from "@/app/projects/TableView";
 import ModalNewTask from "@/components/ModalNewTask";
 
 type Props = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 const Project = ({ params }: Props) => {
-  const { id } = params;
+  const { id } = use(params);
   console.log("Project ID:", id);
   const [activeTab, setActiveTab] = useState("Board");
   const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
